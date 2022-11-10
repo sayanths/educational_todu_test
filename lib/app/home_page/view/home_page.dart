73837e2eea0389f3a8cde2904acb7054f606979f
sayanth A <sayanthsayu619@gmail.com>
2022-11-10 09:44:52 +0530

    ui todo list starting

--- lib/app/home_page/view/home_page.dart
+++ lib/app/home_page/view/home_page.dart
@@ -8,11 +8,15 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: const Icon(
+          Icons.menu,
+          color: Colors.black,
+        ),
         elevation: 0,
         title: Text(
           "Personal Manager",
           style: GoogleFonts.lato(
-              fontSize: 25,
+              fontSize: 20,
               letterSpacing: 2,
               color: Colors.black,
               fontWeight: FontWeight.bold),
@@ -27,7 +31,70 @@ class HomePage extends StatelessWidget {
       ),
       body: SafeArea(
         child: Column(
-          children: const [],
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Welcome",
+              style: GoogleFonts.lato(
+                  fontSize: 30,
+                  letterSpacing: 2,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  height: 60,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                Container(
+                  height: 60,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                Container(
+                  height: 60,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Todo List",
+              style: GoogleFonts.lato(
+                  fontSize: 20,
+                  letterSpacing: 2,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 50),
+              child: Divider(
+                height: 20,
+                color: Colors.black,
+                thickness: 1,
+              ),
+            ),
+          ],
         ),
       ),
     );
